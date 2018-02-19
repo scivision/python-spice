18cebc6e1d6489a49fce06482f317c6436c3ff78
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-19 18:02:19 -0500

    interp

--- ledDrop.py
+++ ledDrop.py
@@ -7,7 +7,7 @@ from scipy.interpolate import interp1d
 # %% brightness from XHP50 datasheet
 B = [.2, .4, .6, .8, 1, 1.2]
 I = [.1, .25,.4, .55, .7, .85]
-f = interp1d(I,B)
+f = interp1d(I,B,'cubic')
 # %% run sim
 ret = subprocess.check_output(['gnucap','-b','ledDrop.net'], universal_newlines=True)
 dat = pandas.read_csv(io.StringIO(ret),header=10,delimiter='\s+').squeeze()
